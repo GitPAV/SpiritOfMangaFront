9be4e193364555820e7cc9a2cdad9a12af1f2f78
Nitrix - Valentin ROGER <valent.23@hotmail.fr>
2019-06-03 11:21:58 +0200

    init backOffice branch

---
+++ src/app/backOffice/pages/back-office-page/back-office-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-back-office-page',
+  templateUrl: './back-office-page.component.html',
+  styleUrls: ['./back-office-page.component.scss']
+})
+export class BackOfficePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
