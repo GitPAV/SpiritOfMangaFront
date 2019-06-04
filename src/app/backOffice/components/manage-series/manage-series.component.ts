9108aac8d8e643b90595ec7f47b606394f467b0d
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-06-04 14:18:21 +0200

    adding the input field and forms

--- src/app/backOffice/components/manage-series/manage-series.component.ts
+++ src/app/backOffice/components/manage-series/manage-series.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from './../../../services/user-service.service'
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 
   
@@ -11,17 +13,45 @@ import { UserServiceService } from './../../../services/user-service.service'
 })
 export class ManageSeriesComponent implements OnInit {
 
-  constructor( private userService: UserServiceService) { }
+  constructor( private fb: FormBuilder, private userService: UserServiceService) { }
+
+  manageSeriesForm = this.fb.group({
+    nameSeries: ['', Validators.required],
+    photoCover: ['', Validators.required],
+    types_id: ['', Validators.required],
+    descriptionSeries: ['', Validators.required],
+  });
+
+  nameSerie= '';
+  photoSerie= '';
+  descriptionSerie= '';
+  searchText = '';
 
   series;
-  searchText = '';
+  types;
+  
   
   ngOnInit() {
     this.userService.getSeries().subscribe(
       series => { this.series = series;
+    });
+    
+    this.userService.getTypes().subscribe(
+      types => { this.types = types;
       });
+  };
+
+  getTheSerie(serie){
+    this.nameSerie = serie.nameSeries
+    this.photoSerie = serie.photoCover
+    this.descriptionSerie = serie.description
   }
 
-  
+  onSubmit() {
+    //Call the observable in service with the apropiate http method 
+
+    const seriesRoute = 'series/manage-series';
+    this.userService.testPut(this.manageSeriesForm.value, seriesRoute).subscribe();
+  }
   
 }
