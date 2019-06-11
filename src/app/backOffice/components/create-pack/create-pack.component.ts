fdd590ee1f0c757529b906ce78591fbde32996e4
Nitrix - Valentin ROGER <valent.23@hotmail.fr>
2019-06-11 14:50:38 +0200

    working form with db no styles yet

--- src/app/backOffice/components/create-pack/create-pack.component.ts
+++ src/app/backOffice/components/create-pack/create-pack.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { UserServiceService} from '../../../services/user-service.service';
+
 
 @Component({
   selector: 'app-create-pack',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePackComponent implements OnInit {
 
-  constructor() { }
+  createPackForm = this.fb.group({
+    namePack: ['', Validators.required],
+    photoPack: ['', Validators.required],
+    resumePack: ['', Validators.required],
+    stock: ['', Validators.required],
+    weight: ['', Validators.required],
+    prixHT: ['', Validators.required],
+    TVA: ['', Validators.required],
+    prixTTC: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder, private userService: UserServiceService) { }
 
   ngOnInit() {
   }
+
+  onSubmit() {
+    // Call the observable in service with the apropiate http method
+
+    const seriesRoute = 'packs/manage-packs';
+    this.userService.testPost(this.createPackForm.value, seriesRoute).subscribe();
+  }
+
+
 
 }
