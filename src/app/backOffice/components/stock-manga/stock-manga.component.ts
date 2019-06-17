5a107384ab854c943d3b1b3c22aaceb49c90ba0c
Julia Hu <juliahukoshi@gmail.com>
2019-06-17 12:11:52 +0200

    wip

--- src/app/backOffice/components/stock-manga/stock-manga.component.ts
+++ src/app/backOffice/components/stock-manga/stock-manga.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Mangas } from 'src/app/common/models/manga.model';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { UserServiceService } from 'src/app/services/user-service.service';
+import { StatesService } from 'src/app/services/states.service';
 
 @Component({
   selector: 'app-stock-manga',
@@ -9,9 +11,17 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class StockMangaComponent implements OnInit {
   mangaTitle: Mangas[];
-
   stockForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  stockUrl = 'http://localhost:4242/statesMangas/manage-states-stock';
+  states = [];
+  promoTable=[
+    {name:"oui",boolean:true},
+    {name:"non",boolean:false}
+  ]
+
+  constructor(private fb: FormBuilder, 
+    private dbService: UserServiceService, 
+    private statesService: StatesService) { }
 
   ngOnInit() {
     this.stockForm = this.fb.group({
@@ -25,13 +35,28 @@ export class StockMangaComponent implements OnInit {
       TVA:['',Validators.required],
       prixTTC:['',Validators.required],
     })
+
+    this.statesService.getStates()
+      .subscribe( states => {
+        this.states = states;
+      }
+    )
   }
 
   getTheChosenManga(event){   
     this.mangaTitle = event;
-    //console.log(this.mangaTitle)
   }
 
+  onSubmit(){
+    this.stockForm.get("mangas_id").patchValue(this.mangaTitle[0].id);
+    console.log(this.stockForm.value);
+    this.dbService.testPost(this.stockForm.value, this.stockUrl);
+    this.stockForm.reset();
+  }
 
+  // getTheTVA(){
+  //   this.TVA = 5
+  //   prixHt * TVA / 100
+  // }
 
 }
