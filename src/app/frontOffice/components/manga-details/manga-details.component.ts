a2c3fed79dd52410fd8482465a25598a8eb1f164
Nitrix - Valentin ROGER <valent.23@hotmail.fr>
2019-07-09 16:20:18 +0200

    wip mangaDetails component

--- src/app/frontOffice/components/manga-details/manga-details.component.ts
+++ src/app/frontOffice/components/manga-details/manga-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MangaDataService } from '../../../services/manga-data.service';
+
 
 @Component({
   selector: 'app-manga-details',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MangaDetailsComponent implements OnInit {
 
-  constructor() { }
+  mangas = [];
+  i = 0; // remplacer 0 par l'ID du manga selectionné
+  choosenManga = [];
+
+  constructor( private mangadataservice: MangaDataService) { }
 
   ngOnInit() {
+    this.mangadataservice.getMangas().subscribe(mangas => {
+      this.mangas = mangas;
+      // Change la value du I par l'ID du manga selectionné
+      this.choosenManga = this.mangas[this.i];
+      console.log(this.choosenManga);
+    });
+
   }
 
 }
