0bf2a5f3fbda956e44103e27d8107e209e0934a5
Nitrix - Valentin ROGER <valent.23@hotmail.fr>
2019-07-16 11:08:21 +0200

    wip pack details

--- src/app/frontOffice/components/packs-details/packs-details.component.ts
+++ src/app/frontOffice/components/packs-details/packs-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { GetPacksService } from '../../../services/get-packs.service';
+
 
 @Component({
   selector: 'app-packs-details',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PacksDetailsComponent implements OnInit {
 
-  constructor() { }
+  packs = [];
+
+  choosenPack;
+  packsID;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private GetPacksService: GetPacksService,
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      this.GetPacksService.getPacks().subscribe(packs => {
+        this.packs = packs;
+        this.packsID = parseInt(params.get('packID')); // Récupère ID du pack
+        this.choosenPack = this.packs[this.packsID - 1]; // Récupère le pack by ID
+        console.log(this.choosenPack);
+        
+      });
+    });
+
   }
 
 }
+
